2c2a789e00a7ddb4e07c92153a64f954d82df8b2
daniarques01 <126002384+daniarques01@users.noreply.github.com>
2023-02-22 12:36:47 +0100

    Update infor.m
    Solución de errores y sólo factorial de números positivos y el 0.

--- infor.m
+++ infor.m
@@ -2,16 +2,15 @@
 clear all
 clc
 
-num=input('Introduzca un número entero:)
-
-factorial=1
-
-for i=1:num
-    factorial=factorial*i
-end
-
-if num>=0
-    disp(['El factorial de',num2str(num),'es:',factorial])
+num=input('Introduzca un número entero:');
+factorial=1;
+if num<0
+    disp('No existe el factorial de un número negativo')
+elseif num==0
+    disp(['El factorial de ',num2str(num),' es: ',num2str(factorial)])
 else
-    disp(['El factorial de',num,'es:',factorial])
+    for i=1:num
+        factorial=factorial*i;
+    end
+    disp(['El factorial de ',num2str(num),' es: ',num2str(factorial)])
 end
